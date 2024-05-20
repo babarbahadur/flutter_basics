819214037cecdae027cf1fca02bbebaeb9602761
nbahadur@nilfisk.com <nbahadur@nilfisk.com>
2024-05-20 12:49:45 +0200

    Colour const added as variable for Gradient component

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:first_app/text_component.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colorList});
+
+  final List<Color> colorList;
 
   @override
   Widget build(context) {
     return Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.cyan, Colors.deepOrangeAccent],
+            colors: colorList,
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           ),
         ),
-        child: const Center(child: TextComponent()));
+        child: const Center(child: TextComponent('Kaam pe ho?')));
   }
 }
--- lib/text_component.dart
+++ lib/text_component.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class TextComponent extends StatelessWidget {
-  const TextComponent({super.key});
+  const TextComponent(this.title, {super.key});
+
+  final String title;
 
   @override
   Widget build(context) {
-    return const Text('Hello World',
-        style: TextStyle(
+    return Text(title,
+        style: const TextStyle(
             fontFamily: AutofillHints.creditCardExpirationDate,
             color: Colors.white,
             fontSize: 30));
